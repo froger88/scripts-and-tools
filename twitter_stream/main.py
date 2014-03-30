8678c793dfe2a28a5258c40f3eae9c65dd940dca
froger88 <froger@froger.pl>
2014-03-30 22:44:24 +0200

    run

--- twitter_stream/main.py
+++ twitter_stream/main.py
@@ -4,6 +4,9 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import json
+import pymongo
+
 import config
 
 api_key=config.api_key
@@ -12,13 +15,27 @@ api_secret=config.api_secret
 access_token=config.access_token
 access_token_secret=config.access_token_secret
 
+
+mongocl = pymongo.MongoClient()['mongo-1']
+counter = 0
+
 class StdOutListener(StreamListener):
     """ A listener handles tweets are the received from the stream.
     This is a basic listener that just prints received tweets to stdout.
 
     """
     def on_data(self, data):
-        print type(data)
+        global counter
+        global mongocl
+        if counter == config.max_count:
+            print "finished"
+            exit(0)
+
+        counter += 1
+        json_data = json.loads(data)
+        #print json_data
+        mongocl[config.task_id].insert(json_data)
+        print "\r%d" % counter,
         return True
 
     def on_error(self, status):
@@ -30,4 +47,4 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
 
     stream = Stream(auth, l)
-    stream.filter(track=config.keywords, languages = config.languages)
+    stream.filter(track=config.keywords, languages=config.languages)
